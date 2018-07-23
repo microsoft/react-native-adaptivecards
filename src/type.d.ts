68d8d442901552723b7e27a208c02bf24c206611
Dongyu Zhao <donz@microsoft.com>
2018-07-23 08:51:07 +0000

    Merged PR 710118: Update type define for AdpativeCard
    Update type define for AdpativeCard

--- src/type.d.ts
+++ src/type.d.ts
@@ -5,6 +5,10 @@ interface AdaptiveCardsProps {
     adaptiveCard: any;
     overrideStyle?: DecStyleConfig;
     onSubmit?: (data: any) => void;
+    onOpenUrl?: (url: string) => void;
+    onCallback?: (url: string, parameters: { [key: string]: string }) => Promise<any>;
+    onFocus?: () => void;
+    onBlur?: () => void;
 }
 
 declare const AdaptiveCardView: ComponentClass<AdaptiveCardsProps>;
