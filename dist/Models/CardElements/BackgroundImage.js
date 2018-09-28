fc39ce86c7401b806b3258dca9ac0159cf30350a
Dongyu Zhao <donz@microsoft.com>
2018-09-28 06:28:04 +0000

    Merged PR 785462: FIx crash in chrome debug
    FIx crash in chrome debug

--- dist/Models/CardElements/BackgroundImage.js
+++ dist/Models/CardElements/BackgroundImage.js
@@ -1,7 +1,6 @@
 import { AbstractModel } from '../Abstract/AbstractModel';
 export class BackgroundImageModel extends AbstractModel {
     constructor(json, parent, context) {
-        json.type = 'BackgroundImage';
         super(json, parent, context);
         this.url = json.url;
         this.mode = json.mode;
--- src/Models/CardElements/BackgroundImage.ts
+++ src/Models/CardElements/BackgroundImage.ts
@@ -2,13 +2,13 @@ import { CardContext } from '../../Contexts/CardContext';
 import { AbstractModel } from '../Abstract/AbstractModel';
 
 export class BackgroundImageModel extends AbstractModel {
+    public type: 'BackgroundImage';
     public url: string;
     public mode: 'stretch' | 'repeatHorizontally' | 'repeatVertically' | 'repeat';
     public horizontalAlignment: 'left' | 'center' | 'right';
     public verticalAlignment: 'top' | 'center' | 'bottom';
 
     constructor(json: any, parent: AbstractModel, context: CardContext) {
-        json.type = 'BackgroundImage';
         super(json, parent, context);
 
         this.url = json.url;
