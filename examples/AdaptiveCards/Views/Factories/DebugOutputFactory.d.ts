912efdce7c370d0b6cd0c522e6d209d725a2edc4
Vince <572151+faceach@users.noreply.github.com>
2019-09-17 18:02:35 +0800

    Support RN v0.60.0, use react-native-webview instead RN build-in WebView (#93)

--- examples/AdaptiveCards/Views/Factories/DebugOutputFactory.d.ts
+++ examples/AdaptiveCards/Views/Factories/DebugOutputFactory.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 export declare class DebugOutputFactory {
     static createDebugOutputBanner(info: string, theme: 'default' | 'emphasis', level: 'info' | 'warning' | 'error' | 'success'): JSX.Element;
 }
